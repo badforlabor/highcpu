3df2452f2ec33eb99487cc37e4230e56e4d3dd61
liubo04 <liubo04@work.com>
2020-02-24 15:30:06 +0800

    2

--- main.go
+++ main.go
@@ -12,23 +12,29 @@ import (
 	"time"
 )
 
-var s = flag.Int("s", 1, "memory size")
+var m = flag.Int("m", 1, "memory size")
+var mc = flag.Int("mc", 1, "memory count")
 var t = flag.Int("t", 1, "thread count")
 var mid = flag.Int("mid", 1, "mid cpu")
 var sleep = flag.Int("sleep", 1, "mid cpu")
 func main() {
 	flag.Parse()
-	fmt.Println(*s, *t)
+	fmt.Println(*m, *t)
 
-	var size = 1024 * 1024 * 1024 * *s
-	var mem = make([]byte, size, size)
+	var size = 1024 * 1024 * 1024 * *m
+	var memList = make([][]byte, *mc)
+	for i,_ := range memList {
+		memList[i] = make([]byte, size, size)
+	}
 	var counter = 0
 	if *t > 1 {
 		for i:=0; i<*t-1; i++ {
 			go func() {
 				for {
 					counter++
-					mem[0] = 1
+					for im, _ := range memList {
+						memList[im][0] = 1
+					}
 				}
 			}()
 		}
@@ -38,7 +44,9 @@ func main() {
 		var cnt = 0
 		for cnt < 1024 * 1024 * 1 * *mid {
 			counter++
-			mem[0] = 1
+			for im, _ := range memList {
+				memList[im][0] = 1
+			}
 			cnt++
 		}
 		time.Sleep(time.Microsecond * time.Duration(*sleep))
